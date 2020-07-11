109ac91723ebdba07e266ac05de501ebfcd9dd87
mmboyce <mathieumimmsboyce@gmail.com>
2020-07-11 09:31:18 -0400

    Add jsdoc.

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,15 +6,37 @@ import PropTypes from 'prop-types';
 
 import './Search.css';
 
-// Object containing values for getItem from localStorage
+/**
+ * This enum contains keys for localStorage.
+ * @enum {string}
+ */
 const listKeys = {
   value: 'listKeyValue',
 };
 
+/**
+ * A helper function to determine whether an item in the Autocomplete dropdown menu should
+ * render.
+ *
+ * It checks to see if what is in the textbox is found within the name of the pokemon.
+ * @param {Object} pokemon The pokemon to compare inputValue against.
+ * @param {string} pokemon.name
+ * @param {string} pokemon.url
+ * @param {string} pokemon.id
+ * @param {string} inputValue The text the user has put in the searchbox.
+ * @returns {boolean} If the user's input matches the current pokemon.
+ * @example
+ * <Autocomplete shouldItemRender={handleMatchPokemon} />
+ */
 function handleMatchPokemon(pokemon, inputValue) {
   return pokemon.name.indexOf(inputValue.toLowerCase()) !== -1;
 }
 
+/**
+ * A helper function that returns a capitalized version of a supplied string.
+ * @param {string} toCapitalize The uncapitalized text.
+ * @returns {string} The capitalized version of the supplied string.
+ */
 function capitalizeString(toCapitalize) {
   const split = toCapitalize.split('');
 
@@ -23,11 +45,27 @@ function capitalizeString(toCapitalize) {
   return split.join('');
 }
 
+/**
+ * The Search Component displays a textbox which redirects to the Pokemon with the matching name
+ * entered in its field. As the user types into the textbox, an autocomplete search appears which
+ * also redirects when a name is selected.
+ * @component
+ * @class Search
+ * @extends {React.Component}
+ * @example
+ * const results = [{}]
+ *
+ * <Search results={results} />
+ */
 class Search extends React.Component {
   constructor(props) {
     super(props);
 
+    /** The value in localStorage that represents what was in the search input when the user's
+     * previous session ended.
+    */
     const cachedValue = localStorage.getItem(listKeys.value);
+    /** The text stored in the search input field of the component. */
     const value = cachedValue === null ? '' : cachedValue;
 
     this.state = {
@@ -39,6 +77,13 @@ class Search extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
+  /**
+   * This function sets state.value to match what is in the textbox of the component.
+   * @param {Event} e The onChange event that triggered this method.
+   * @memberof Search
+   * @example
+   * <Autocomplete onChange={this.handleChange} />
+   */
   handleChange(e) {
     const { value } = e.target;
 
@@ -50,6 +95,16 @@ class Search extends React.Component {
     localStorage.setItem(listKeys.value, value);
   }
 
+  /**
+   * This function is triggered when the user either submits the text in the search box, or when
+   * they select a pokemon from the autocomplete dropdown.
+   *
+   * It seeks a match based on the input, and if it finds one it sets the redirect state to match
+   * that pokemon's id.
+   * @memberof Search
+   * @example
+   * <Autocomplete onSelect={this.handleSearch} />
+   */
   handleSearch() {
     const { results } = this.props;
     const { value } = this.state;
@@ -61,6 +116,11 @@ class Search extends React.Component {
       findId = match.id;
     }
 
+    // Why set localStorage again if it is set whenever a change is made?
+    // This is to account for when the user has clicked on a Pokemon's name in the dropdown
+    // instead of typing it out fully, since that doesn't trigger a change event.
+    localStorage.setItem(listKeys.value, value);
+
     this.setState({
       redirect: findId,
     });
@@ -70,6 +130,7 @@ class Search extends React.Component {
     const { redirect, value } = this.state;
     const { results } = this.props;
 
+    /** If state.redirect is false, no redirect occurs, but if it is an ID we redirect to that ID */
     const redirectTo = redirect === false ? (<></>) : (<Redirect to={`/${redirect}`} />);
 
     return (
@@ -85,6 +146,10 @@ class Search extends React.Component {
           getItemValue={(pokemon) => pokemon.name}
           shouldItemRender={handleMatchPokemon}
           onChange={this.handleChange}
+          /** Once the a value is submitted or a dropdown has been selected, the text in the
+           * textbox is formatted to be capitalized, and handleSearch is called as a callback
+           * of setState to hanlde the redirect to the submitted pokemon.
+           */
           onSelect={(input) => this.setState({ value: capitalizeString(input) }, this.handleSearch)}
           renderMenu={(children) => (
             <div className="menu">
